ba1521682582c3bfa59de1e74d918e3c73fd5416
slaython <slaython.gleyson@gmail.com>
2024-07-27 20:06:35 -0300

    adição de logos e loading

--- src/app/demo/authentication/admin/admin.component.ts
+++ src/app/demo/authentication/admin/admin.component.ts
@@ -2,25 +2,31 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
+import { LoadingComponent } from '../../loading/loading.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'admin',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule, LoadingComponent],
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
 export default class AdminComponent {
   email: string = '';
   password: string = '';
+  loading: boolean = false;
 
   constructor(private authService: AuthService) {}
   // REQUISIÇÃO DE LOGIN
   login() {
+    this.loading = true;
     this.authService.login(this.email, this.password).subscribe(response => {
       console.log('Login successful', response);
+      this.loading = false;
     }, error => {
       console.error('Login failed', error);
+      this.loading = false;
     });
   }
 
